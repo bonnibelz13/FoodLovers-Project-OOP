1e5857e00ad9b1e6cf53a36a891cae8ecaa61ef2
Noppakorn Boonmee <117189340+kaonoppa@users.noreply.github.com>
2023-05-16 16:23:53 +0700

    HomeGUI

---
+++ FoodLover_Kao/FoodLover_Kao/src/HomeModel.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class HomeModel {
+    private ArrayList latest, recommended;
+    private String category;
+    
+    public HomeModel(){
+        
+    }
+    
+}
---
+++ FoodLover_Kao/FoodLover_Kao/src/Main.java
@@ -0,0 +1,8 @@
+
+public class Main {
+    public static void main(String[] args) {
+        HomeMenuView view = new HomeMenuView();
+        view.setLocationRelativeTo(null);
+        view.setVisible(true);
+    }
+}
